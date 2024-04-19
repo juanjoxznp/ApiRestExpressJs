fb3fd0c1178f8c784de469672417f52a89ac8256
Juanjoxznp <juanjoxznp@gmail.com>
2024-04-19 13:19:41 -0500

    Deploy

--- api/index.js
+++ api/index.js
@@ -27,11 +27,11 @@ const options = {
 }
 app.use(cors(options))
 
-app.get('/', (req, res) => {
+app.get('/api', (req, res) => {
   res.send('Hola mi server en Express');
 });
 
-app.get('/nueva-ruta', (req, res) => {
+app.get('/api/nueva-ruta', (req, res) => {
   res.send('Hola, soy una nueva ruta');
 });
 
